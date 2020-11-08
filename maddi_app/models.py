fb7d15f9f7fdd29d2ff0db19905a54e4bb954ad2
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-11-09 01:33:28 +0700

    update model and migration

--- maddi_app/models.py
+++ maddi_app/models.py
@@ -19,23 +19,46 @@ class Item(models.Model):
   price = models.IntegerField()
   description = models.TextField()
   image = fields.ImageField(upload_to='item')
-  
-class Purchase(models.Model):
-  user = models.OneToOneField(Customer, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class Courrier(models.Model):
+  name = models.CharField(max_length=40)
+
+class Review(models.Model):
+  user = models.ForeignKey(Customer, on_delete=models.CASCADE)
   item = models.ForeignKey(Item, on_delete=models.CASCADE)
+  rating = models.FloatField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class Purchase(models.Model):
+  user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+  items = models.ManyToManyField('Item', through='Cart', through_fields=('purchase', 'item'))
   destination_address = models.CharField(max_length=250)
   destination_city = models.PositiveIntegerField()
+  cancellation = models.SmallIntegerField()
+  created_at = models.DateTimeField(auto_now_add=True)
+
+class Cart(models.Model):
+  purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+  item = models.ForeignKey(Item, on_delete=models.CASCADE)
   quantity = models.IntegerField()
   message = models.TextField()
-  cancellation = models.SmallIntegerField()
+  total_price = models.BigIntegerField()
 
 class Shipping(models.Model):
   purchase = models.OneToOneField(Purchase, on_delete=models.CASCADE)
   status = models.CharField(max_length=20)
+  courrier = models.ForeignKey(Courrier, on_delete=models.CASCADE)
+  shipping_price = models.BigIntegerField()
+  date_shipped = models.DateTimeField()
+  date_arrived = models.DateTimeField()
 
 class Payment(models.Model):
   purchase = models.OneToOneField(Purchase, on_delete=models.CASCADE)
-  total_price = models.IntegerField()
+  payment_amount = models.BigIntegerField()
   date_paid = models.DateTimeField()
   method = models.CharField(max_length=30)
   status = models.CharField(max_length=30)
+  updated_at = models.DateTimeField(auto_now=True)
